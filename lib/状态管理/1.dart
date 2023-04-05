953c0bd4ab8d0bb6785f89196c65955714608f10
shen杨 <1571817467@qq.com>
2023-04-06 01:57:02 +0800

    Flutter study

---
+++ lib/状态管理/1.dart
@@ -0,0 +1,12 @@
+/*1、状态管理
+*
+* 状态：需要维护的数据
+* build方法依赖状态渲染widget
+*
+* 1、短时状态 Ephemeral state====>只在自己的widget中使用(单一)
+* 2、应用状态 App state=====>在多个部件中传递共享
+*
+*
+* 共享状态管理
+* InheritedWidget可以实现跨组件之间的数据传递
+* Provider*/
